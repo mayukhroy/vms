c6ca62d849d9288018ac3a769ee59ba9675c5ab9
mayukhroy <mayukhr@smartek21.com>
2014-05-16 00:29:36 -0700

    gitignore added config/environments/development.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,12 +1,16 @@
 Library::Application.configure do
-  # Settings specified here will take precedence over those in config/application.rb
+ # Settings specified here will take precedence over those in config/application.rb
 
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
-
+  config.assets.initialize_on_precompile = false
+  # Log error messages when you accidentally call methods on nil.
+  config.whiny_nils = true
+  #config.action_mailer.default_url_options = { host =>'localhost',port:3000 }
   config.action_mailer.default_url_options = { host: 'localhost:3000' }
+  
   config.action_mailer.delivery_method = :smtp
     config.action_mailer.delivery_method = :smtp
 	config.action_mailer.smtp_settings = {
@@ -16,13 +20,12 @@ Library::Application.configure do
 	  tls: true,
 	  domain:'google.com', #you can also use google.com
 	  authentication: 'plain',
-	  user_name: "amitsinha559@gmail.com",
-	  password: "papiyasinha3__"
+	  user_name: "nitbabar@gmail.com",
+	  password: "lakeshore123"
 	}
   
-  #config.action_mailer.perform_deliveries = false
- 
-  
+  config.secret_key = '781e411f9b20de1fb09f24a4858ffce9df2da5b7bd20c6533b6529b0bfce2af09826b53a44b63e45d49f564f0e338899277dbcced4ce827439e181eca29021b3'
+
   # Log error messages when you accidentally call methods on nil.
   config.whiny_nils = true
 
