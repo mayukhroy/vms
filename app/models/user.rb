4cc36d71d2beeec58971554dbe556649e9a1b2af
root <root@nitin-VirtualBox.(none)>
2014-05-19 12:36:19 +0530

    Module      :: User Description :: validations to new fields name and contact number in signup form Author      :: Nitin Date        :: 19th May 14

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,5 +11,8 @@ devise :database_authenticatable, :registerable,
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :name, :contact_number, :remember_me, :role
   # attr_accessible :title, :body
+  validates :name, length: { in: 4..20 }
+  validates :contact_number, length: { in: 0..10 },numericality: { only_integer: true }
+  
 end
 # :confirmable
