831d1421bb0e37344eb2b16903d8dfd3ad38bf1b
mayukhroy <mayukhr@smartek21.com>
2014-05-21 06:56:06 -0700

    Purpose: added a method in helper to seperate the logic from view file Modified By: mayukhroy

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+def getServiceVendorName(vendor_id)
+	return Vendor.find(vendor_id).name
+end
 end
--- app/models/vendor.rb
+++ app/models/vendor.rb
@@ -1,7 +1,8 @@
 class Vendor < ActiveRecord::Base
-  attr_accessible :email, :name, :user_id
+  attr_accessible :email, :name, :user_id, :address, :contact_no
   has_many :services, :dependent => :destroy
   belongs_to :user
   has_many :projects, :through => :invitations
   #dependent: :destroy
+  validates :email, :uniqueness => true
 end
