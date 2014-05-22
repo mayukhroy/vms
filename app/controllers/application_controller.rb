4373c4f04abcb16b4faedf7d187fb5d2df224b55
nitin <nitin@nitin-VirtualBox.(none)>
2014-05-23 01:06:44 +0530

    Route handlers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,17 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   #layout 'template'
+rescue_from ActiveRecord::RecordNotFound, :with => :not_found
+rescue_from ActionController::RoutingError, :with => :rescue_404
+
+  def not_found
+   redirect_to :controller=>'vendors', :action=>'index'
+  end
+  
+  def rescue_404
+     redirect_to :controller=>'vendors', :action=>'index'
+  end
+   
 end
 
 
