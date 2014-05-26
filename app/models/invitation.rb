b31fc30a577ab56e2ae5890e022f5dd19419e742
amitsinha559 <amitsinha559@gmail.com>
2014-05-26 18:01:30 +0530

    added relationship --- invitation

--- app/models/invitation.rb
+++ app/models/invitation.rb
@@ -1,3 +1,6 @@
 class Invitation < ActiveRecord::Base
   attr_accessible :invitation_sent_at, :invitee_id, :inviter_id, :project_id, :vendor_id
+  belongs_to :vendor
+  belongs_to :project
+  belongs_to :user
 end
