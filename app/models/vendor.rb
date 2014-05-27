3fda61ac9f3dee861c14e1a2e43d9a709810c320
niloferb <nilofersumaya@gmail.com>
2014-05-27 19:21:16 +0530

    nilofer
    vendor

--- app/models/vendor.rb
+++ app/models/vendor.rb
@@ -3,6 +3,7 @@ class Vendor < ActiveRecord::Base
   has_many :services, :dependent => :destroy
   belongs_to :user
   has_many :projects, :through => :invitations
-  #dependent: :destroy
-  validates :email, :uniqueness => true
+ #dependent: :destroy
+  validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+  validates :name, :presence => true
 end
